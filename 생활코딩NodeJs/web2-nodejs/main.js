665111147bdf6c3b0a6db522400a32e2308d6a83
BeomSu-Kang <kbs21c1987@gmail.com>
2022-08-14 20:23:14 +0900

    query 사용하기

--- 생활코딩NodeJs/web2-nodejs/main.js
+++ 생활코딩NodeJs/web2-nodejs/main.js
@@ -1,20 +1,26 @@
 let http = require("http");
 let fs = require("fs");
+let url = require("url");
+
+//require 요구하다.
 
 let app = http.createServer((request, response) => {
-  let url = request.url;
-  if (request.url === "/") {
-    url = "/index.html";
+  let _url = request.url;
+  // console.log(_url);
+  let queryData = url.parse(_url, true).query;
+  console.log(queryData.id);
+
+  if (_url === "/") {
+    _url = "/index.html";
   }
 
-  if (request.url === "/favicon.ico") {
+  if (_url === "/favicon.ico") {
     return response.writeHead(404);
   }
 
   response.writeHead(200);
 
-  console.log(__dirname + url);
-  response.end(fs.readFileSync(__dirname + url));
+  response.end(queryData.id); //
   //response.end() 웹브라우저의 요청에 응답하는 명령어.
 });
 app.listen(4000);
