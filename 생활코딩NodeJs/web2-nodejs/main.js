e54bc39e4a54e392ccede0f44c2bc1b4605eeabc
BeomSu-Kang <kbs21c1987@gmail.com>
2022-08-15 20:25:19 +0900

    readFile 적용하기

--- 생활코딩NodeJs/web2-nodejs/main.js
+++ 생활코딩NodeJs/web2-nodejs/main.js
@@ -22,7 +22,8 @@ let app = http.createServer((request, response) => {
 
   response.writeHead(200);
 
-  let template = `
+  fs.readFile(`data/${queryData.id}.txt`, "utf8", (err, description) => {
+    let template = `
   <!doctype html>
   <html>
   <head>
@@ -37,16 +38,13 @@ let app = http.createServer((request, response) => {
       <li><a href="/?id=JavaScript">JavaScript</a></li>
     </ul>
     <h2>${title}</h2>
-    <p><a href="https://www.w3.org/TR/html5/" target="_blank" title="html5 speicification">Hypertext Markup Language (HTML)</a> is the standard markup language for <strong>creating <u>web</u> pages</strong> and web applications.Web browsers receive HTML documents from a web server or from local storage and render them into multimedia web pages. HTML describes the structure of a web page semantically and originally included cues for the appearance of the document.
-    <img src="coding.jpg" width="100%">
-    </p><p style="margin-top:45px;">HTML elements are the building blocks of HTML pages. With HTML constructs, images and other objects, such as interactive forms, may be embedded into the rendered page. It provides a means to create structured documents by denoting structural semantics for text such as headings, paragraphs, lists, links, quotes and other items. HTML elements are delineated by tags, written using angle brackets.
-    </p>
+    <p>${description}</p>
   </body>
   </html>
   
   `;
-
-  response.end(template);
+    response.end(template);
+  });
   //response.end() 웹브라우저의 요청에 응답하는 명령어.
 });
 app.listen(3000);
